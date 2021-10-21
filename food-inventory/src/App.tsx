b34cea5cde48ac5515830a0b2c624b2a0a77b657
Andreea Daniela Manole <sirpurple2016@yahoo.com>
2021-10-22 00:31:43 +0300

    [first page] stylized first page and added new mui components
    note: could not center components properly

--- food-inventory/src/App.tsx
+++ food-inventory/src/App.tsx
@@ -3,6 +3,11 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { Button, FormControl, IconButton, Input, InputAdornment, InputLabel, Link, Stack, TextField } from '@mui/material';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import LockRoundedIcon from '@mui/icons-material/LockRounded';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -11,25 +16,91 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+
 export default function BasicGrid() {
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop: string): (event: any) => void => {
+    return (event) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+  };
+
   return (
-    <Box sx={{ pt: "15%" , pl:"15%" }}>
-    <Box display="grid" gridTemplateColumns="repeat(11, 1fr)" gap={2}>
-      <Box gridRow="span 4" gridColumn="span 6">
-        <Item sx={{ m: "10%" }}>Login picture</Item>
+    <Box sx={{ pt: "5%" , pl:"5%" , pr:"5%" , pb:"5%"}} bgcolor="purple" alignItems="center" justifyContent="center">
+    <Box display="grid" gridTemplateColumns="repeat(11, 1fr)" gap={2} bgcolor="white" alignItems="center" justifyContent="center" >
+      <Box gridRow="span 4" gridColumn="span 6" >
+       <img src= "user.png" width = "200" height="200"></img>
       </Box>
-      <Box gridColumn="span 3" >
+      <Box gridColumn="span 4" >
         <Item>Login Page</Item>
       </Box>
-      <Box gridColumn="span 3">
-        <Item>Username</Item>
+      <Box gridColumn="span 4" sx={{ '& > :not(style)': { m: 1 } }}>
+        <FormControl variant="standard" fullWidth>
+          <InputLabel htmlFor="input-with-icon-adornment">
+            Username or email
+          </InputLabel>
+          <Input
+            id="input-with-icon-adornment"
+            startAdornment={
+              <InputAdornment position="start">
+                <EmailRoundedIcon/>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </Box>
-      <Box gridColumn="span 3">
-        <Item>Password</Item>
+      
+      <Box gridColumn="span 4">
+      <FormControl sx={{ m: 1}} variant="standard" fullWidth>
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            startAdornment={
+              <InputAdornment position="start">
+                <LockRoundedIcon/>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </Box>
-      <Box gridColumn="span 3">
-        <Item>Login</Item>
-      </Box>
+      <Stack  gridColumn="span 4" direction="column" alignItems="center" spacing={1}>
+        <Button variant="outlined" fullWidth  style={{textTransform: 'none', color: "blue"}}>Login</Button>
+        <Link href="#">Forgot password ?</Link>
+      </Stack>
+     
     </Box>
   </Box>
   );
